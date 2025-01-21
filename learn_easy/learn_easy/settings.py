d727c11b670f8d53671c203cc1ee0a7067b9087a
abdoulaye-96 <107267177+abdoulaye-96@users.noreply.github.com>
2025-01-21 13:35:26 +0100

    le setup

--- learn_easy/learn_easy/settings.py
+++ learn_easy/learn_easy/settings.py
@@ -16,12 +16,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m8r6s=vshzl+gd@260rsl8m!n!*mb!#p(29$1jd51bk-wn*ixn'
+SECRET_KEY = 'django-insecure-0@%6hcr^ehv1d(o12%el1patoqu+x(&1s@4t$@xph+@=05xhl_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,30 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # Applications Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    # Applications du projet
-    'core',
-    
-    # Django Rest Framework
-    'rest_framework',
 ]
-
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-    ],
-    'DEFAULT_PARSER_CLASSES': [
-        'rest_framework.parsers.JSONParser',
-    ]
-}
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -72,7 +54,7 @@ ROOT_URLCONF = 'learn_easy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -93,12 +75,8 @@ WSGI_APPLICATION = 'learn_easy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydatabase',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -143,20 +121,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'core.CustomUser'
-
-LOGIN_REDIRECT_URL = '/dashboard/'
-LOGOUT_REDIRECT_URL = '/'
-
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'core/static',
-]
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# media
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
